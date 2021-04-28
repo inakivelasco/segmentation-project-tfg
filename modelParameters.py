9738158420fdb8a0d2102cb034d70c266802e51f
inakivelasco <77887322+IVR10@users.noreply.github.com>
2021-04-28 20:45:47 +0200

    Add files via upload

---
+++ modelParameters.py
@@ -0,0 +1,22 @@
+models = ['model256x512_RGB', 'model256x512_HSV', 'model256x512_Lab', 'model256x512_RGB_HSV_Lab', 'model256x512_RGB_LBP', 'model256x512_RGB_SP']
+
+def returnModelParams(modelName):
+    epochs = 20
+    lr = 1e-4 
+    batchSize = 4
+    nClasses = 4
+    
+    if modelName == 'model256x512_RGB':
+        shape = (256, 512, 3)
+    elif modelName == 'model256x512_HSV':
+        shape = (256, 512, 3)
+    elif modelName == 'model256x512_Lab':
+        shape = (256, 512, 3)
+    elif modelName == 'model256x512_RGB_HSV_Lab':
+        shape = (256, 512, 9)
+    elif modelName == 'model256x512_RGB_LBP':
+        shape = (256, 512, 4)
+    elif modelName == 'model256x512_RGB_SP':
+        shape = (256, 512, 3)
+    
+    return nClasses, shape, lr, batchSize, epochs
